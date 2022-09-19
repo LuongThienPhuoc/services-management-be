0a1fc8510a9d2cbe34b91678ea6a93aed57d5d5d
Luong Thien Phuoc <phuoc.t.luong@taptap.com.vn>
2022-09-19 09:42:34 +0700

    done

--- src/routers/service.js
+++ src/routers/service.js
@@ -2,6 +2,7 @@ const express = require("express")
 const router = express.Router()
 const serviceController = require("../controllers/serviceController")
 
+router.get("/check-deadlock/:id", serviceController.checkDeadlock)
 router.get("/get-all-service", serviceController.getAllService)
 router.post("/add-new-service", serviceController.addNewService)
 
